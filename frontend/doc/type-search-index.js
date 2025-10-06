955d4285c03c572b5a676c566863d5265c0b4ede
Rodrigo Fernandez <r.fernandez.mattiaude@gmail.com>
2025-10-06 03:38:32 -0300

    Docs + fix pom.xml

---
+++ frontend/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"turismouyapp.servlets","l":"DbServerPublish"},{"p":"turismouyapp.servlets","l":"Login"}];updateSearchResults();
